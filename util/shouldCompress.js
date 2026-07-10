d4db0456c45328b0463bd88a07aeb79704d0a0f6
GracePranto <gracepranto02@gmail.com>
2026-07-11 05:28:34 +0600

    Update shouldCompress.js

--- util/shouldCompress.js
+++ util/shouldCompress.js
@@ -1,13 +1,12 @@
-const MIN_COMPRESS_LENGTH = 1024;
-const MIN_TRANSPARENT_COMPRESS_LENGTH = 102400;
-
-function shouldCompress(imageType, size, isTransparent) {
-  return !(
-    !imageType.startsWith("image") ||
-    size === 0 ||
-    (isTransparent && size < MIN_COMPRESS_LENGTH) ||
-    (!isTransparent && (imageType.endsWith("png") || imageType.endsWith("gif")) && size < MIN_TRANSPARENT_COMPRESS_LENGTH)
-  );
+function shouldCompress(imageType, size) {
+  // If it's not an image, don't touch it
+  if (!imageType.startsWith("image")) return false;
+  
+  // If it's smaller than 1KB (1024 bytes), ignore it 
+  if (size < 1024) return false; 
+  
+  // Otherwise, compress everything
+  return true; 
 }
 
 module.exports = shouldCompress;
